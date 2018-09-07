c5bf77dfccec084c3c105aa130c20878305056d0
deju <deju.tu@gmail.com>
2018-09-07 14:24:59 +0800

    feat(deploy): deploy by pm2

--- shells/deploy.sh
+++ shells/deploy.sh
@@ -4,6 +4,6 @@ if [[ "$PWD" =~ shells ]];then
     cd ../
 fi
 
-servers=(172.28.49.2:8888)
+# (yarn install --production  && npm run build) || exit -1
 
-(yarn install --production  && npm run build) || exit -1
+pm2 start npm --name "react-crm-kit" --update-env -- run server 
