9925bba237a739ef6836e9810c226be4e36042ac
deju <deju.tu@gmail.com>
2018-09-13 20:25:57 +0800

    fix(intl-polyfill): remove intl package, use polyfill intl

--- src/js/polyfills/intl.js
+++ src/js/polyfills/intl.js
@@ -2,8 +2,9 @@
 // 针对ie9浏览器
 
 if (!window.Intl) {
-    require('intl');
-    // require('intl/locale-data/complete.js');
-    require('intl/locale-data/jsonp/en.js');
-    require('intl/locale-data/jsonp/zh-Hans-CN.js');
+    // see int index.pug file
+    // require('intl');
+    // // require('intl/locale-data/complete.js');
+    // require('intl/locale-data/jsonp/en.js');
+    // require('intl/locale-data/jsonp/zh-Hans-CN.js');
 }
